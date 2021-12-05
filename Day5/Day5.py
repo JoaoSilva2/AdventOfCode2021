880deea5ea35f9956ce0156b730e91be5ab48cdd
JoaoSilva2 <jdhsilva2000@gmail.com>
2021-12-05 15:34:17 +0000

    refactored code for day 5

--- Day5/Day5.py
+++ Day5/Day5.py
@@ -1,5 +1,26 @@
 import sys
 
+def horizontal_line(positions, x1, x2, y1, step_x):
+    for i in range(x1, x2+step_x, step_x):
+        if (i, y1) not in positions.keys():
+            positions[(i, y1)] = 1
+        else:
+            positions[(i, y1)] += 1
+
+def vertical_line(positions, y1, y2, x1, step_y):
+    for i in range(y1, y2+step_y, step_y):
+        if (x1, i) not in positions.keys():
+            positions[(x1, i)] = 1
+        else:
+            positions[(x1, i)] += 1
+
+def diagonal_line(positions, x1, x2, y1, y2, step_x, step_y):
+    for (x, y) in zip(range(x1, x2+step_x, step_x), range(y1, y2+step_y, step_y)):
+        if (x, y) not in positions.keys():
+            positions[(x, y)] = 1
+        else:
+            positions[(x, y)] += 1
+
 def check_line_intersection(file_lines, diagonal):
     positions = {}
     count = 0
@@ -10,33 +31,16 @@ def check_line_intersection(file_lines, diagonal):
         step_y = (y2-y1)//max(abs(y2-y1), 1)
         step_x = (x2-x1)//max(abs(x2-x1), 1)
         if x1 == x2:
-            for i in range(y1, y2+step_y, step_y):
-                if (x1, i) not in positions.keys():
-                    positions[(x1, i)] = 1
-                else:
-                    positions[(x1, i)] += 1
+            vertical_line(positions, y1, y2, x1, step_y)
         elif y1 == y2:
-            for i in range(x1, x2+step_x, step_x):
-                if (i, y1) not in positions.keys():
-                    positions[(i, y1)] = 1
-                else:
-                    positions[(i, y1)] += 1
+            horizontal_line(positions, x1, x2, y1, step_x)
         elif diagonal:
-            while x1 != x2+step_x:
-                if (x1, y1) not in positions.keys():
-                    positions[(x1, y1)] = 1
-                else:
-                    positions[(x1, y1)] += 1
-                x1 += step_x 
-                y1 += step_y
+            diagonal_line(positions, x1, x2, y1, y2, step_x, step_y)
     
-
     for key in positions.keys():
         if positions[key] > 1:
             count += 1
     return count
-
-
 
 if __name__ == "__main__":
     file = open(sys.argv[1], "r")
